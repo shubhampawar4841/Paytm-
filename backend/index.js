ae2bc45cf633db8ccc42dca50adc4ee6a5148bd5
shubhampawar4841 <shubhampawar4036@gmail.com>
2025-02-09 11:35:02 +0530

    added database, routes, tokens

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express();
+const rootRouter = require("./routes/index");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use("/api/v1",rootRouter);
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
