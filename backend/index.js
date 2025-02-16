a9af52f661f957d1bd6165b634aacbd52f687cd6
shubhampawar4841 <shubhampawar4036@gmail.com>
2025-02-16 19:18:20 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const accountRoutes = require("./routes/accountRoutes");
 
-app.use(cors("*"));
+app.use(cors({ origin: "http://localhost:5175", credentials: true })); // Allow frontend domain
+
 
 app.use(express.json());
 require("dotenv").config();
